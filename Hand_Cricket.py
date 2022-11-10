0f52bc17c4cd7eaae22c3417d2b0537d2ff06d0b
NilanshuRanjan <116020416+NilanshuRanjan@users.noreply.github.com>
2022-11-10 15:34:51 +0530

    Update Hand_Cricket.py

--- Hand_Cricket.py
+++ Hand_Cricket.py
@@ -7,11 +7,11 @@ a = random.choice(l)
 def toss():
     ch = input("odd or even")
     if ch in "eE":
-        n = int(input("enter your number"))
+        n = int(input("enter your number: "))
         print("computer's number", a)
         if (n + a) % 2 == 0:
 
-            bat_ball = input("enter for bat or ball")
+            bat_ball = input("enter bat or ball: ")
             if bat_ball == "bat":
                 bat()
             else:
@@ -25,10 +25,10 @@ def toss():
             else:
                 bat()
     elif ch in "oO":
-        n = int(input("enter your number"))
+        n = int(input("enter your number: "))
         print("computer chose", a)
         if (n + a) % 2 != 0:
-            bat_ball = input("enter for bat or ball")
+            bat_ball = input("enter bat or ball: ")
             if bat_ball == "bat":
                 bat()
             else:
@@ -50,7 +50,7 @@ def bat():
     global p1
 
     while True:
-        n1 = int(input("enter your shot"))
+        n1 = int(input("enter your shot: "))
         n2 = random.choice(l)
         print("computer threw", n2)
         if n1 != n2:
@@ -68,7 +68,7 @@ def ball():
     global p2
 
     while True:
-        n3 = int(input("throw your ball"))
+        n3 = int(input("throw your ball: "))
         n4 = random.choice(l)
         print("computer hits", n4)
         if n3 != n4:
@@ -83,7 +83,7 @@ def s_bat():
     global p1
 
     while True:
-        n5 = int(input("enter your shot"))
+        n5 = int(input("enter your shot: "))
         n6 = random.choice(l)
         print("computer threw", n6)
         if n5 != n6:
@@ -100,7 +100,7 @@ def s_ball():
     global p2
 
     while True:
-        n7 = int(input("Throw your ball"))
+        n7 = int(input("Throw your ball: "))
         n8 = random.choice(l)
         print("computer threw", n8)
         if n7 != n8:
